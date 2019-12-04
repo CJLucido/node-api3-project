afca5544b583cca306eacfa0fa40b739d42c665c
CJ Lucido <c.jose.lucido@gmail.com>
2019-12-04 14:50:42 -0800

    MVP and 2 files made, can now compare with and without custom middleware

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ const helmet = require('helmet')
 
 //LOCAL FILES (ROUTERS)
 const userRouter = require('./users/userRouter')
-
+const userRouterCopy = require('./users/userRouterCopy')
 const postRouter = require('./posts/postRouter')
 
 //BUILD SERVER POWERED BY EXPRESS
@@ -17,7 +17,7 @@ server.use(logger)
 
 //routers
 server.use("/api/posts", postRouter)
-server.use('/api/users', userRouter)
+server.use('/api/users', userRouterCopy)
 
 //custom middleware
 
