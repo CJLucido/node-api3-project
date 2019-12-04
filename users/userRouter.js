8a1fa30e4d047870d716ab7e8b34113bc9819228
CJ Lucido <c.jose.lucido@gmail.com>
2019-12-04 13:34:35 -0800

    crud on userRouter complete except put and posts

--- users/userRouter.js
+++ users/userRouter.js
@@ -2,7 +2,7 @@
 const express = require('express');
 
 //LOCAL FILES
-const Posts = require('./userDb')
+const Users = require('./userDb')
 
 //ESTABLISH ROUTER
 const router = express.Router();
@@ -11,6 +11,7 @@ const router = express.Router();
 
 router.post('/', (req, res) => {
   // do your magic!
+  
 });
 
 router.post('/:id/posts', (req, res) => {
@@ -19,18 +20,86 @@ router.post('/:id/posts', (req, res) => {
 
 router.get('/', (req, res) => {
   // do your magic!
+  Users.get()
+  .then(users => {
+    res.status(200).json(users)
+  })
+  .catch(err => {
+    console.log('GET users all err', err)
+    res.status(500).json({error: "Error fetching all users, server"})
+  })
 });
 
 router.get('/:id', (req, res) => {
   // do your magic!
+  const userId = req.params.id
+
+  Users.getById(userId)
+  .then(user => {
+    if(!user){
+      res.status(404).json({error: "User does not exist"})
+    }else{
+      res.status(200).json(user)
+    }
+    
+  })
+  .catch(err => {
+    console.log('GET single user err', err)
+    res.status(500).json({error: "Error specific user, server"})
+  })
 });
 
 router.get('/:id/posts', (req, res) => {
   // do your magic!
+  const userId = req.params.id
+
+  Users.getById(userId)
+  .then(user => {
+
+    if(!user){
+      res.status(404).json({error: "User not found"})
+    }else{
+      Users.getUserPosts(userId)
+      .then(posts => {
+        res.status(200).json(posts)
+      })
+      .catch(err => {
+        console.log('GET user posts err', err)
+        res.status(500).json({error: "Error retrieving posts, server"})
+      })
+    }
+    
+  })
+  .catch(err => {
+    console.log('GET single user err', err)
+    res.status(500).json({error: "Error specific user, server"})
+  })
+
 });
 
 router.delete('/:id', (req, res) => {
   // do your magic!
+  const userId = req.params.id
+
+  Users.getById(userId)
+  .then(user => {
+    if(!user){
+      res.status(404).json({error: "User not found"})
+    }else{
+      Users.remove(userId)
+      .then(countOfRemoved => {
+        res.status(200).json(`Number of records removed ${countOfRemoved}`)
+      })
+      .catch(err => {
+        console.log("DEL user error on remove function", err)
+        res.status(500).json({error: "Error attempting to remove"})
+      })
+    }
+  })
+  .catch(err => {
+    console.log("DEL user error on user ids", err)
+    res.status(500).json({error: "Error attempting to find user on DEL"})
+  })
 });
 
 router.put('/:id', (req, res) => {
@@ -38,7 +107,7 @@ router.put('/:id', (req, res) => {
 });
 
 
-//MIDDLEWARE
+//MIDDLEWARE all local
 //custom middleware
 
 function validateUserId(req, res, next){
